7047652596ccbccc4ac73b249c439e1adc961354
Jason Patton <jpatton@cs.wisc.edu>
2024-11-15 15:01:10 -0600

    INF-2484 Use same JSON writer as log generator

--- tests/artifacts/referencedata/ospool/dedupe_xdmod_logs.py
+++ tests/artifacts/referencedata/ospool/dedupe_xdmod_logs.py
@@ -1,6 +1,9 @@
 import json
+import shutil
 import argparse
+from os import fsync
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 
 def parse_args():
@@ -8,6 +11,26 @@ def parse_args():
     parser.add_argument("--today", required=True, type=Path, help="today's log file")
     parser.add_argument("--yest", required=True, type=Path, help="yesterday's log file")
     return parser.parse_args()
+
+
+def write_json_file(obj, path: Path, indent=None):
+    with NamedTemporaryFile(mode="w", delete=False) as f:
+        tmp_path = Path(f.name)
+        json.dump(obj, f, indent=indent)
+        f.flush()
+        fsync(f.fileno())
+    try:
+        path.parent.mkdir(parents=True, exist_ok=True)
+        try:  # try atomic move first
+            tmp_path.rename(path)
+        except OSError:
+            shutil.move(tmp_path.as_posix(), path.as_posix())
+    except OSError:
+        try:
+            tmp_path.unlink()
+        except FileNotFoundError:
+            pass
+        raise
 
 
 def main():
@@ -24,7 +47,7 @@ def main():
     print(f"Found {len(new_logs)} unique logs today")
 
     deduped_today = args.today.with_name(f"deduped.{args.today.name}")
-    json.dump(new_logs, deduped_today.open("w"))
+    write_json_file(new_logs, deduped_today, indent=2)
     print(f"Wrote deduped logs to {str(deduped_today)}")
 
 
